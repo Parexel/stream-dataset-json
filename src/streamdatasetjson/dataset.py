3cc69d2e74da790a5ca894c3d61fa659a382e172
CimimUxMaio <cimimuxmaio@gmail.com>
2022-09-09 16:10:22 -0300

    Improvements, typings, dataset complete metadata, list of available datasets

--- src/streamdatasetjson/dataset.py
+++ src/streamdatasetjson/dataset.py
@@ -1,34 +1,45 @@
+from typing import Any, Iterable, List, NewType
 import utils
 
-class Dataset:
-    def __init__(self, dataset_json_file, name):
-        self._df = dataset_json_file
-        self._name = name
-        self._dataset_prefix = f"clinicalData.itemGroupData.{self.name}"
 
+Row = NewType("Row", List[str])
+VarMeta = NewType("VarMeta", dict)
+
+class Dataset:
+    """
+    Dataset. Should not be instantiated manually.
+    """
+    def __init__(self, dataset_json_file, name, prefix):
+        self._df = dataset_json_file
+        self._prefix = prefix
+        self._name = name
         self._records = self._get_attribute("records")
         self._label   = self._get_attribute("label")
         self._items   = self._get_attribute("items")
 
     def _get_attribute(self, attr_name):
-        return utils.load_prefix(self._df, f"{self._dataset_prefix}.{attr_name}")
+        return utils.load_prefix(self._df, f"{self._prefix}.{attr_name}")
     
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
     @property
-    def observations(self):
-        return utils.reread_prefix(f"{self._dataset_prefix}.itemData.item")
+    def observations(self) -> Iterable[Row]:
+        return utils.reread_prefix(self._df, f"{self._prefix}.itemData.item")
 
     @property
-    def records(self):
+    def records(self) -> int:
         return self._records
 
     @property
-    def label(self):
+    def label(self) -> str:
         return self._label
 
     @property
-    def items(self):
+    def items(self) -> VarMeta:
         return self._items
+
+    @property
+    def variables(self) -> List[str]:
+        return [ meta["name"] for meta in self.items ]
--- src/streamdatasetjson/datasetjson.py
+++ src/streamdatasetjson/datasetjson.py
@@ -1,3 +1,5 @@
+from typing import List
+import ijson
 import utils
 
 from dataset import Dataset
@@ -5,8 +7,12 @@ from errors import DatasetNotFoundError
 
 
 class DatasetJSON:
+    """
+    Dataset JSON instance. 
+    """
     def __enter__(self):
         self._file = open(self._path, "r")
+        self._dataset_prefixes = self._generate_dataset_prefixes()
         return self
 
     def __exit__(self, type, value, traceback):
@@ -15,8 +21,23 @@ class DatasetJSON:
     def __init__(self, path):
         self._path = path
 
-    def get_dataset(self, target_name):
-        try:
-            return Dataset(self._file, target_name)
-        except StopIteration:
+    def get_dataset(self, target_name: str) -> Dataset:
+        if target_name not in self._dataset_prefixes.keys():
             raise DatasetNotFoundError(target_name)
+
+        return Dataset(self._file, target_name, self._dataset_prefixes[target_name])
+    
+    @property
+    def available_datasets(self) -> List[str]:
+        return self._dataset_prefixes.keys()
+
+    def _generate_dataset_prefixes(self):
+        events = ijson.parse(self._file)
+
+        prefixes = []
+        for prefix, event, value in events:
+            if prefix == "clinicalData.itemGroupData" or prefix == "referenceData.itemGroupData":
+                if event == "map_key":
+                    prefixes.append(f"{prefix}.{value}")
+
+        return { utils.load_prefix(self._file, f"{prefix}.name"): prefix for prefix in prefixes }
