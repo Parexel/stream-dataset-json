97fe36a06c1acacdcec4c4bf3e0be62658d0d2ac
CimimUxMaio <cimimuxmaio@gmail.com>
2022-09-21 14:39:42 -0300

    First attempt to one scan only constructor method

--- src/streamdatasetjson/dataset.py
+++ src/streamdatasetjson/dataset.py
@@ -1,6 +1,7 @@
 from io import TextIOWrapper
 from typing import Iterable, NamedTuple, NewType, Optional
 import streamdatasetjson.utils as utils
+import ijson
 
 
 Row = NewType("Row", 'list[str]')
@@ -23,14 +24,34 @@ class Dataset:
     def __init__(self, dataset_json_file: JSONFileObject, name: str, prefix: str):
         self._df = dataset_json_file
         self._prefix = prefix
-        self._name = name
-        self._records = self._get_attribute("records")
-        self._label = self._get_attribute("label")
-        self._items = [self._raw_to_item(raw_item)
-                       for raw_item in self._get_attribute("items")]
 
-    def _get_attribute(self, attr_name: str):
-        return utils.load_prefix(self._df, f"{self._prefix}.{attr_name}")
+        self._name = name
+        self._records = None
+        self._label = None
+        self._items = []
+
+        self._df.seek(0)
+        item, item_key = None, None
+        for prefix, event, value in ijson.parse(self._df):
+            if not prefix.startswith(self._prefix):
+                continue
+
+            sub_prefix = prefix.replace(self._prefix, "")
+
+            if sub_prefix == ".records":
+                self._records = value
+            elif sub_prefix == ".label":
+                self._label = value
+            elif sub_prefix == ".items.item":
+                if event == "start_map":
+                    item = {}
+                elif event == "end_map":
+                    self._items.append(item)
+                elif event == "map_key":
+                    item_key = value
+            elif item_key and sub_prefix == f".items.item.{item_key}":
+                item[item_key] = value
+                item_key = None
 
     def _raw_to_item(self, raw_item: dict) -> Item:
         return Item(oid=raw_item["OID"],
