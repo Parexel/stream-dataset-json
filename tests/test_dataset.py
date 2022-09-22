d3546f2c1fd7360ca4bd7bec75c2eeeb055a5d1b
CimimUxMaio <cimimuxmaio@gmail.com>
2022-09-22 12:52:25 -0300

    Dataset.items specific tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,18 +2,15 @@ import pytest
 import streamdatasetjson as dj
 
 
-ADAE_PATH = "./examples/simple_dataset.json"
+SIMPLE_DATASET_PATH = "./examples/simple_dataset.json"
 MULTIPLE_DATASETS_PATH = "./examples/multiple_datasets.json"
 NON_EXISTENT_PATH = "./examples/non_existent.json"
 
 
 class TestDatasetLoadedProperties:
-    simple_dataset = None
-    multi_dataset = None
-
     @pytest.fixture(scope="session", autouse=True)
     def fixture(self):
-        simple_json = dj.DatasetJSON(ADAE_PATH)
+        simple_json = dj.DatasetJSON(SIMPLE_DATASET_PATH)
         multi_json = dj.DatasetJSON(MULTIPLE_DATASETS_PATH)
         simple_dataset = simple_json.get_dataset("DM")
         multi_dataset = multi_json.get_dataset("AE")
@@ -52,3 +49,19 @@ class TestDatasetLoadedProperties:
     def test_correctly_loads_the_variables_property_for_jsons_with_multiple_datsets(self, fixture):
         (_, multi_dataset) = fixture
         assert multi_dataset.variables == ["ITEMGROUPDATASEQ", "STUDYID", "USUBJID", "DOMAIN", "AGE"]
+
+
+class TestDatasetItemsProperty:
+    def test_correctly_loads_all_attributes_for_each_of_the_items_property_for_jsons_with_one_dataset(self):
+        with dj.DatasetJSON(SIMPLE_DATASET_PATH) as json:
+            simple_dataset = json.get_dataset("DM")
+            first_item = simple_dataset.items[0]
+            assert first_item.oid == "ITEMGROUPDATASEQ" and first_item.name == "ITEMGROUPDATASEQ" \
+                and first_item.label == "Record identifier" and first_item.type == "integer"
+
+    def test_correctly_loads_all_attributes_for_each_of_the_items_property_for_jsons_with_multiple_datasets(self):
+        with dj.DatasetJSON(MULTIPLE_DATASETS_PATH) as json:
+            multi_dataset = json.get_dataset("AE")
+            first_item = multi_dataset.items[0]
+            assert first_item.oid == "ITEMGROUPDATASEQ" and first_item.name == "ITEMGROUPDATASEQ" \
+                and first_item.label == "Record identifier" and first_item.type == "integer"
