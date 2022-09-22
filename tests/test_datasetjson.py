397e99c765e4f1955ef5d529ca89dc178683786c
CimimUxMaio <cimimuxmaio@gmail.com>
2022-09-22 10:19:02 -0300

    Working tests with updated directory structure

--- tests/test_datasetjson.py
+++ tests/test_datasetjson.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 import streamdatasetjson as dj
 
 
@@ -7,31 +7,32 @@ MULTIPLE_DATASETS_PATH = "./examples/multiple_datasets.json"
 NON_EXISTENT_PATH = "./examples/non_existent.json"
 
 
-class TestDatasetJSONAvailableDatasets(unittest.TestCase):
+class TestDatasetJSONAvailableDatasets:
     def test_correctly_returns_all_available_datasets_when_json_contains_just_one_dataset(self):
         with dj.DatasetJSON(ADAE_PATH) as json:
-            self.assertListEqual(json.available_datasets, ["ADAE"])
+            assert json.available_datasets == ["ADAE"]
 
     def test_correctly_returns_all_available_datasets_when_json_contains_multiple_datasets(self):
         with dj.DatasetJSON(MULTIPLE_DATASETS_PATH) as json:
-            self.assertListEqual(json.available_datasets, ["DM", "AE"])
+            assert json.available_datasets == ["DM", "AE"]
 
     def test_fails_if_the_given_json_path_is_invalid(self):
         # TODO
         pass
 
 
-class TestDatasetJSONGetDataset(unittest.TestCase):
+class TestDatasetJSONGetDataset:
     def test_correctly_returns_the_required_dataset_when_json_contains_just_one_dataset(self):
         with dj.DatasetJSON(ADAE_PATH) as json:
             dataset = json.get_dataset("ADAE")
-            self.assertEqual(dataset.name, "ADAE")
+            assert dataset.name == "ADAE"
 
     def test_correctly_returns_the_required_dataset_when_json_contains_multiple_datasets(self):
         with dj.DatasetJSON(MULTIPLE_DATASETS_PATH) as json:
             dataset = json.get_dataset("AE")
-            self.assertEqual(dataset.name, "AE")
+            assert dataset.name == "AE"
 
     def test_fails_if_the_given_dataset_name_does_not_exist_in_the_given_json(self):
         with dj.DatasetJSON(ADAE_PATH) as json:
-            self.assertRaises(dj.DatasetNotFoundError, lambda: json.get_dataset("NON_EXISTENT"))
+            with pytest.raises(dj.DatasetNotFoundError):
+                json.get_dataset("NONT_EXISTENT_DATASET")
